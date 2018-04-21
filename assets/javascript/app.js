01da882aeb4934152b89f6830950c68b35401215
paullnh <plaird83@gmail.com>
2018-04-21 13:08:22 -0400

    For JS build is working on form submit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,33 @@
+var now = Date(2018 - 01 - 01);
+
 var employee = {
   name: "Paul",
   role: "Engineer",
-  startDate: "01/01/18",
-  rate: 15000
+  startDate: Date(2017 - 01 - 01),
+  monthsWorked: 6,
+  rate: 15000,
+  totalBilled: 60000
 };
-var monthsWorked,
-  totalBilled,
-  now = moment().date();
 
-$("#submit").on("click", function() {
-  monthsWorked = now - employee.startDate;
-  console.log(monthsWorked);
-  var th = $("<th>");
-  var tdName = $("<td>" + eomployee.name + "</td>");
+$("#submitForm").on("click", function(event) {
+  event.preventDefault();
+  console.log(employee.monthsWorked);
+  var th = $('<th scope="row">');
+  var tr = $("<tr>");
+  var tdName = $("<td>" + employee.name + "</td>");
   var tdRole = $("<td>" + employee.role + "</td>");
   var tdStart = $("<td>" + employee.startDate + "</td>");
-  var tdMonths = $("<td>" + monthsWorked + "</td>");
+  var tdMonths = $("<td>" + employee.monthsWorked + "</td>");
   var tdRate = $("<td>" + employee.rate + "</td>");
-  var tdBilled = $("<td>" + monthsWorked * employee.rate + "</td>");
-});
+  var tdBilled = $("<td>" + employee.totalBilled + "</td>");
 
-<tr>
-  <th scope="row">1</th>
-  <td>Paul</td> employeeName
-  <td>Engineer</td> role
-  <td>01/01/18</td> startDate
-  <td>4</td> months Worked (now - start date)
-  <td>$15,000</td> rate
-  <td>$60,000</td> titalBilled
-</tr>;
+  th.text("1");
+  tr.append(tdName);
+  tr.append(tdRole);
+  tr.append(tdStart);
+  tr.append(tdMonths);
+  tr.append(tdRate);
+  tr.append(tdBilled);
+  tr.prepend(th);
+  $("#newTable").append(tr);
+});
